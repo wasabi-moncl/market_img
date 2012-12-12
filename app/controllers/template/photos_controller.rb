ffc5ca2bce974b748f1cb59e76438a92c6b22528
Wonsup Lee <onesup.lee@gmail.com>
2012-12-12 11:11:48 +0900

    add 'part' in template/photos edit

--- app/controllers/template/photos_controller.rb
+++ app/controllers/template/photos_controller.rb
@@ -1,5 +1,6 @@
+#encoding: utf-8
 class Template::PhotosController < ApplicationController
-  before_filter :the_user
+  before_filter :the_template
   def index
     @template = Template.find(params[:template_id])
     @photos = @template.photos
@@ -11,7 +12,7 @@ class Template::PhotosController < ApplicationController
 
 
   def show
-    @photo = Photo.find(params[:id])
+    @photo = @template.photos.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
@@ -43,29 +44,31 @@ class Template::PhotosController < ApplicationController
   end
   
   def edit
-    @photos = @user.photos
+    @photo = @template.photos.find(params[:id])
   end
   
   def update
-    @photos = @user.photos.find(params[:photo_ids])
+    @photo = @template.photos.find(params[:id])
+
     respond_to do |format|
-      if @photos.update_attributes(params[:photos_ids])
-        format.html { redirect_to @photos, notice: 'Item was successfully updated.' }
+      if @photo.update_attributes(params[:photo])
+        format.html { redirect_to [@template, @photo], notice: '사진 was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @photos.errors, status: :unprocessable_entity }
+        format.json { render json: [@template, @photo].errors, status: :unprocessable_entity }
       end
     end
   end
 
 
   def destroy
-    @photo = Photo.find(params[:id])
+    @template = Template.find(params[:template_id])
+    @photo = @template.photos.find(params[:id])
     @photo.destroy
 
     respond_to do |format|
-      format.html { redirect_to gallery_photos_path(@item) }
+      format.html { redirect_to template_photos_path(@template) }
       format.json { head :no_content }
     end
   end
@@ -74,10 +77,6 @@ end
 
 private 
 
-def the_user
-  if logged_in?
-    @user = current_user
-  else
-    redirect_to login_path
-  end
+def the_template
+  @template = Template.find(params[:template_id])
 end
