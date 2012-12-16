da2e471d526720fecc8b9fd681fe442f306c61d4
Minsoo Kim <m@msk.pe.kr>
2012-12-17 07:30:18 +0900

    photos seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@
 User.destroy_all
 Template.destroy_all
 Position.destroy_all
+Photo.destroy_all
 
 users = User.create(
   [
@@ -45,4 +46,18 @@ template = Template.create(
 end
 puts'"ilcorso" 템플릿 생성완료'
 Position.association_to_the_template
+
+
+photos = ['MC1G2D401DB.png',   'MC1G2D403GE_1.png',  'MCD02D409BL.png',    'MCD02D503GE_1.png',
+'MC1G2D401DB_1.png',  'MC1G2D403MT.png',    'MCD02D409BL_1.png',  'MCD02D503KK.png',
+'MC1G2D402GE.png',    'MC1G2D403MT_1.png',  'MCD02D409IV.png',    'MCD02D503KK_1.png',
+'MC1G2D402GE_1.png',  'MCD01D908NV.png',    'MCD02D409IV_1.png',  'MCH02C301DI.png',
+'MC1G2D403GE.png',    'MCD01D908NV_1.png',  'MCD02D503GE.png',    'MCH02C301DI_1.png' ]
+
+photos.each do |x|
+  Photo.create(:photo_file => open(Rails.root.to_s + "/db/photos/" + x), :user_id => User.first.id)
+end
+
+  
+  
 
