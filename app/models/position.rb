2d12729a11e069d910216b24f14f0cb8a0d418d6
Wonsup Lee <onesup.lee@gmail.com>
2013-01-15 15:22:21 +0900

    geometry for mini_magick

--- app/models/position.rb
+++ app/models/position.rb
@@ -12,4 +12,25 @@ class Position < ActiveRecord::Base
     end
     puts "associate " + template.positions.count.to_s + " items to template"
   end
+  
+  def geometry
+    if self.x_pos.nil? && self.y_pos.nil?
+      result = "+0+0"
+    elsif self.x_pos.nil? && self.y_pos.class == Fixnum
+      result = "+0" + operation(self.y_pos)
+    elsif self.x_pos.class == Fixnum && self.y_pos.nil?
+      result = operation(self.x_pos) + "+0"
+    else
+      result = operation(self.x_pos) + operation(self.y_pos)
+    end
+    result
+  end
+end
+private
+def operation(number)
+  if number > -1
+    "+" + number.to_s
+  else
+    number.to_s
+  end
 end
--- lib/composer.rb
+++ lib/composer.rb
@@ -1,79 +1,81 @@
 class Composer
-  def self.m(mold)
-    if mold.photos.where(:part => mold.positions.map(&:part)).empty?
-      rails_png = './app/assets/images/rails.png'
-      dst = MiniMagick::Image.open(rails_png)
-    else
-      result_name = 'public/generated_images/mold_' + mold.id.to_s + '_example.png'
-      dst = Magick::Image.new(image_width(mold), image_height(mold))
-      dst.background_color = "#aaaaaa"
-      dst.format = "PNG"
-      dst.write(result_name)
-      mold.positions.order("part desc").each do |position|
-        # unless mold.photos.where(:part => position.part).empty?
-        #   photo = mold.photos.where(:part => position.part).first
-        # else
-        #   photo = example_item.photos.where(:part => position.part).first
-        # end
-        # src = MiniMagick::Image.read('public'+photo.photo_file.url)
-        # result = dst.composite(src, position.x_pos, position.y_pos) do |c|
-        # end
-        # result.write(result_name)
-      end
-    end
-    dst.to_blob
-  end
-  
-  def self.item
-    example_item
-  end
-  
-  def self.image_width(mold)
-    positions = mold.positions
-    x_positions = positions.map(&:x_pos)
-    start_point = x_positions.sort.first
-    end_points = Array.new
-    positions.each do |position|
-      x_position = position.x_pos
-      if position.width.nil?
-        if mold.photos.where(:part => position.part).count > 0
-          photo = mold.photos.where(:part => position.part).first
-          end_points << Magick::Image.read(photo.photo_file.path).first.columns + x_position
-        elsif example_item.photos.where(:part => position.part).count > 0
-          photo = example_item.photos.where(:part => position.part).first
-          end_points << Magick::Image.read(photo.photo_file.path).first.columns + x_position
-        end
+  class << self
+    def m(mold)
+      if mold.photos.where(:part => mold.positions.map(&:part)).empty?
+        rails_png = './app/assets/images/rails.png'
+        dst = MiniMagick::Image.open(rails_png)
       else
-        end_points << position.width + x_position
+        result_name = 'public/generated_images/mold_' + mold.id.to_s + '_example.png'
+        dst = Magick::Image.new(image_width(mold), image_height(mold))
+        dst[0].background_color = "#aaaaaa"
+        dst.format = "PNG"
+        dst.write(result_name)
+        mold.positions.order("part desc").each do |position|
+          unless mold.photos.where(:part => position.part).empty?
+            photo = mold.photos.where(:part => position.part).first
+          else
+            photo = example_item.photos.where(:part => position.part).first
+          end
+          src = MiniMagick::Image.open('public' + photo.photo_file.url)
+          dst = MiniMagick::Image.open(result_name)
+          result = dst.composite(src, "png") do |c|
+            c.geometry ''
+            c.gravity 'NorthEast'
+          end
+          # end
+          # result.write(result_name)
+        end
       end
+      dst.to_blob
     end
-    end_point = end_points.sort.last
-    width = end_point - start_point
-    width
-  end
+  
+    def image_width(mold)
+      positions = mold.positions
+      x_positions = positions.map(&:x_pos)
+      start_point = x_positions.sort.first
+      end_points = Array.new
+      positions.each do |position|
+        x_position = position.x_pos
+        if position.width.nil?
+          if mold.photos.where(:part => position.part).count > 0
+            photo = mold.photos.where(:part => position.part).first
+            end_points << Magick::Image.read(photo.photo_file.path).first.columns + x_position
+          elsif example_item.photos.where(:part => position.part).count > 0
+            photo = example_item.photos.where(:part => position.part).first
+            end_points << Magick::Image.read(photo.photo_file.path).first.columns + x_position
+          end
+        else
+          end_points << position.width + x_position
+        end
+      end
+      end_point = end_points.sort.last
+      width = end_point - start_point
+      width
+    end
 
-  def self.image_height(mold)
-    positions = mold.positions
-    y_positions = positions.map(&:y_pos)
-    start_point = y_positions.sort.first
-    end_points = Array.new
-    positions.each do |position|
-      y_position = position.y_pos
-      if position.height.nil?
-        if mold.photos.where(:part => position.part).count > 0
-          photo = mold.photos.where(:part => position.part).first
-          end_points << Magick::Image.read(photo.photo_file.path).first.rows + y_position
-        elsif example_item.photos.where(:part => position.part).count > 0
-          photo = example_item.photos.where(:part => position.part).first
-          end_points << Magick::Image.read(photo.photo_file.path).first.rows + y_position
+    def image_height(mold)
+      positions = mold.positions
+      y_positions = positions.map(&:y_pos)
+      start_point = y_positions.sort.first
+      end_points = Array.new
+      positions.each do |position|
+        y_position = position.y_pos
+        if position.height.nil?
+          if mold.photos.where(:part => position.part).count > 0
+            photo = mold.photos.where(:part => position.part).first
+            end_points << Magick::Image.read(photo.photo_file.path).first.rows + y_position
+          elsif example_item.photos.where(:part => position.part).count > 0
+            photo = example_item.photos.where(:part => position.part).first
+            end_points << Magick::Image.read(photo.photo_file.path).first.rows + y_position
+          end
+        else
+          end_points << position.height + y_position
         end
-      else
-        end_points << position.height + y_position
       end
+      end_point = end_points.sort.last
+      height = end_point - start_point
+      height
     end
-    end_point = end_points.sort.last
-    height = end_point - start_point
-    height
   end
 end
 private
