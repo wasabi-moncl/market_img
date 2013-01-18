2c408b6ff13d6cb2249f383395c2331c9103b511
Wonsup Lee <onesup.lee@gmail.com>
2013-01-18 09:33:35 +0900

    deploy problem

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,6 +36,7 @@ namespace :deploy do
   task :setup_config, roles: :app do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "ln -nfs #{shared_path}/public/assets #{release_path}/public/assets"
     run "mkdir -p #{shared_path}/config"
     # put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
     puts "Now edit the config files in #{shared_path}."
